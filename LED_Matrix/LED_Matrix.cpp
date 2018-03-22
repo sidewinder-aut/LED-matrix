380c0167d60aa5f67225a5d8a83ac9dde9cd5b70
lucaseder <lucaseder@fvkmb130.vkmthd.tugraz.at>
2018-03-22 15:19:03 +0100

    Prototyping code for testing with parallel adressing of LEDs

--- LED_Matrix/LED_Matrix.cpp
+++ LED_Matrix/LED_Matrix.cpp
@@ -1,36 +1,35 @@
-#include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_NeoPixel.h>
 
-
 // Define output pins for LED strips
-#define pin_for_strip_1 	6
-#define pin_for_strip_2 	7
-
-// Define the two strips (strip objects)
+#define pin_for_strip_1   6
 Adafruit_NeoPixel strip_1 = Adafruit_NeoPixel(10, pin_for_strip_1, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_2 = Adafruit_NeoPixel(10, pin_for_strip_2, NEO_GRB + NEO_KHZ800);
 
 
 void setup() {
+    // Start serial output
+    Serial.begin(9600);
 
-	  // Start serial output
-	  Serial.begin(9600);
-
-
-	  // LED strip setup
-	  strip_1.begin();
-	  strip_1.show(); // Initialize all pixels to 'off'
-	  strip_2.begin();
-	  strip_2.show();
+    // LED strip setup
+    strip_1.begin();
+    strip_1.show(); // Initialize all pixels to 'off'
 }
 
+
 void loop() {
-	for(int i=0; i<10; i++){
-		strip_1.setPixelColor(i, strip_1.Color(125,0,0));
-		strip_1.show();
-		strip_2.setPixelColor(i, strip_2.Color(0,125,0));
-		strip_2.show();
-		delay(1000);
-	}
+  for(int i=0; i<10; i++){
+    if(i<3){
+      strip_1.setPixelColor(i, strip_1.Color(0,0,125));
+      strip_1.show();
+    }
+    if(i>=3 && i<6){
+      strip_1.setPixelColor(i, strip_1.Color(0,125,0));
+      strip_1.show();
+    }
+    if(i>=6 && i<10 ){
+      strip_1.setPixelColor(i, strip_1.Color(125,0,0));
+      strip_1.show();
+    }
+    delay(1000);
+  }
 }
